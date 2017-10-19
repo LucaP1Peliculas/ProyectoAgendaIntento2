2a1d7feb0786e4252f9b6f5d47ec32b39422c1fd
admin <admin@pwc1>
2017-10-19 17:15:18 +0200

    Cambios en clase Telefono, faltan comentarios

--- src/main/java/com/spring/model/Telefono.java
+++ src/main/java/com/spring/model/Telefono.java
@@ -1,11 +1,15 @@
 package com.spring.model;
 
 import java.io.Serializable;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -19,7 +23,10 @@ public class Telefono implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int idtelefonos;
     private String telefono;
-    private int id;
+    
+    @OneToOne(mappedBy="idpersonas",  cascade = CascadeType.ALL)
+    @JoinColumn(name="idpersonas")
+    private Persona idpersonas;
     
     
 	public int getIdtelefonos() {
@@ -34,15 +41,66 @@ public class Telefono implements Serializable {
 	public void setTelefono(String telefono) {
 		this.telefono = telefono;
 	}
-	public int getId() {
-		return id;
+	public Persona getId() {
+		return idpersonas;
 	}
-	public void setId(int id) {
-		this.id = id;
+	public void setId(Persona idpersonas) {
+		this.idpersonas = idpersonas;
 	}
 	
+	/**
+	 * 
+	 */
+    public Telefono(){
+    	
+    }
+    
+    public Telefono(String telefono, Persona idpersonas){
+    	this.telefono=telefono;
+    	this.idpersonas=idpersonas;
+    }
+    
+    
+	@Override
+	public String toString() {
+		return "Telefono [idtelefonos=" + idtelefonos + ", telefono=" + telefono + ", idpersonas=" + idpersonas + "]";
+	}
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idpersonas == null) ? 0 : idpersonas.hashCode());
+		result = prime * result + idtelefonos;
+		result = prime * result + ((telefono == null) ? 0 : telefono.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Telefono other = (Telefono) obj;
+		if (idpersonas == null) {
+			if (other.idpersonas != null)
+				return false;
+		} else if (!idpersonas.equals(other.idpersonas))
+			return false;
+		if (idtelefonos != other.idtelefonos)
+			return false;
+		if (telefono == null) {
+			if (other.telefono != null)
+				return false;
+		} else if (!telefono.equals(other.telefono))
+			return false;
+		return true;
+	}
 	
     
-	
+    
 
 }
