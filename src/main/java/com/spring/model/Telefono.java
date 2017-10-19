b1062c12b27115517c16de468e2f8d3a33a43aad
admin <admin@pwc1>
2017-10-19 16:49:49 +0200

    Intento de subida

--- src/main/java/com/spring/model/Telefono.java
+++ src/main/java/com/spring/model/Telefono.java
@@ -8,10 +8,41 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+@Entity
+@Table(name="telefonos")
+
 public class Telefono implements Serializable {
 	
     private static final long serialVersionUID = 144585071807476496L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int idtelefonos;
+    private String telefono;
+    private int id;
+    
+    
+	public int getIdtelefonos() {
+		return idtelefonos;
+	}
+	public void setIdtelefonos(int idtelefonos) {
+		this.idtelefonos = idtelefonos;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+    
 	
 
 }
