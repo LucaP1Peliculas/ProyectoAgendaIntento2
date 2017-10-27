54c682930d91cbe7dca19979031fd656c133ff76
admin <admin@pwc1>
2017-10-27 08:44:45 +0200

    Versión 0.9.1 [WORKING]

--- src/main/java/com/spring/controller/HomeController.java
+++ src/main/java/com/spring/controller/HomeController.java
@@ -21,12 +21,12 @@ import com.spring.services.IPersonaService;
 
 @Controller
 public class HomeController {
-	
+
 	//Autowired de la capa servicios
 	@Autowired
 	private IPersonaService personaService;
-	
-	
+
+
 	@RequestMapping("/")
 	public ModelAndView handleRequest() throws Exception {
 		System.out.println("Aceptada la respuesta al mapeo /");
@@ -34,38 +34,38 @@ public class HomeController {
 		ModelAndView model = new ModelAndView("index");
 		model.addObject("personList", listPersonas);
 		List<Telefono> t = personaService.getDetalles(1).getTelefonos();
-		
+
 		return model;
 	}
-	
+
 	@RequestMapping(value = "/search")
 	public String Search(@RequestParam("searchString") String searchString, ModelAndView model) {
 
-	    if(searchString != null){
-	        List<Persona> searchResult = personaService.search(searchString);
-	        model.addObject("searchResult", searchResult);  
-	    }
+		if(searchString != null){
+			List<Persona> searchResult = personaService.search(searchString);
+			model.addObject("searchResult", searchResult);  
+		}
 
-	    return "search";
+		return "search";
 	}
-	
+
 	@RequestMapping("/detalles")
 	public ModelAndView handleRequestDetalles(HttpServletRequest request) throws Exception{
-		
-		
+
+
 		int id = Integer.parseInt(request.getParameter("id"));
 		System.out.println("El id del usuario es : " + id);
-		
+
 		System.out.println("-- Entrando en detalles");
 		List<Persona> listPersonas = personaService.list();
-	
+
 		List<Telefono> t = personaService.getDetalles(id).getTelefonos();
 		Telefono telefono2 = t.get(0);
-		
+
 		////////////////CODIGO PABLO/////////////////////
-		
+
 		Telefono telefono1= t.get(1);
-		
+
 		String nombre=personaService.getDetalles(id).getNombre();
 		String apellido1=personaService.getDetalles(id).getApellido1();
 		String apellido2=personaService.getDetalles(id).getApellido2();
@@ -76,17 +76,17 @@ public class HomeController {
 		String cp=d.get(0).getCodPostal();
 		String provincia=d.get(0).getProvincia();
 		String localidad= d.get(0).getLocalidad();
-		
+
 		////////////////////////////////////////////////
-		
-		
+
+
 		ModelAndView model = new ModelAndView("index");
 		model.addObject("personList", listPersonas);
 		model.addObject("telefono2", telefono2);
-		
-		
+
+
 		/////////CODIGO PABLO//////////
-		
+
 		model.addObject("dni", dni);
 		model.addObject("nombre", nombre);
 		model.addObject("apellido1", apellido1);
@@ -98,39 +98,58 @@ public class HomeController {
 		model.addObject("localidad", localidad);
 		model.addObject("telefono1", telefono1);
 		//////////////////////////////
-		
+
 		return model;
 	}
-	
+
 	@RequestMapping(value ="/guardar", method = RequestMethod.POST)
 	public ModelAndView handleRequestGuardar(HttpServletRequest request) throws Exception{
 		System.out.println("-- entrando en guardar");
+
+
 		List<Telefono> listaTelefono = null;
-		
+		List<Direccione> listaDireccion = null;
+
 		Telefono t1 = new Telefono();
 		Telefono t2 = new Telefono();
-		
+
+		Direccione d1 = new Direccione();
+
 		String nombre = request.getParameter("nombre");
 		String apellido1=request.getParameter("apellido1");
 		String apellido2=request.getParameter("apellido2");
 		String fecha=request.getParameter("fechaNacimiento");
-		
-	
+		String dni = request.getParameter("dni");
+
+
 		String direccion= request.getParameter("direccion");
 		String cp=request.getParameter("codPostal");
 		String provincia=request.getParameter("provincia");
 		String localidad= request.getParameter("fechaNacimiento");
 
 		String telefono1 = request.getParameter("telefono1");
-		String telefono2 = request.getParameter("telefono2");		
-		
+		String telefono2 = request.getParameter("telefono2");        
+
 		t1.setTelefono(telefono1);
 		t2.setTelefono(telefono2);
-		
+		d1.setDireccion(direccion);
+		d1.setCodPostal(cp);
+		d1.setProvincia(provincia);
+		d1.setLocalidad(localidad);
+
 		listaTelefono.add(0, t1);
 		listaTelefono.add(1, t2);
-		
-		Persona p = new Persona();
+
+		listaDireccion.add(0, d1);
+
+
+		//
+		//    Persona p = new Persona(apellido1, apellido2,dni,fecha, nombre, listaDireccion,listaTelefono);
+		//    System.out.println("MOSTRAR PERSONA "    + p);
+		// personaService.saveOrUpdate(p);
+
+		/*(String apellido1, String apellido2, String dni, String fechaNacimiento, String nombre,
+        List<Direccione> direcciones, List<Telefono> telefonos) */
 
 		List<Persona> listPersonas = personaService.list();	
 		ModelAndView model = new ModelAndView("index");
@@ -138,7 +157,7 @@ public class HomeController {
 
 		return model;
 	}
-	
+
 
 	@RequestMapping("/inicio")
 	public ModelAndView handleRequestInicio() throws Exception {
@@ -148,7 +167,7 @@ public class HomeController {
 		ModelAndView model = new ModelAndView("index");
 		model.addObject("personList", listPersonas);
 		List<Telefono> t = personaService.getDetalles(1).getTelefonos();
-		
+
 		return model;
 	}
 }
